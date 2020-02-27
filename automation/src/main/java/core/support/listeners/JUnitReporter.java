d0d09ff57476b0fe8b99b7ee8cb1acdd62b7d88b
Ehsan Matean <ehsan.matean@fortify.pro>
2020-02-27 08:41:44 -0800

    cleanedup junitreported

--- automation/src/main/java/core/support/listeners/JUnitReporter.java
+++ automation/src/main/java/core/support/listeners/JUnitReporter.java
@@ -3,44 +3,11 @@ package core.support.listeners;
 import org.testng.ITestResult;
 import org.testng.reporters.JUnitReportReporter;
 
-import core.helpers.Helper;
-import core.support.objects.TestObject;
-
 public class JUnitReporter extends JUnitReportReporter {
-	
 	
 	@Override
 	protected String getTestName(ITestResult tr) {
 		String name = tr.getName();
-		System.out.println(name);
 	    return name;
-	}
-	
-	protected String getTestName2(ITestResult tr) {
-		// Get the Parameters
-		Object[] params = tr.getParameters();
-
-		// Add the method name to the string
-		StringBuilder sb = new StringBuilder();
-		sb.append(tr.getMethod().getMethodName() + " (");
-
-		// Add the parameters to the String
-		String result;
-		if (params.length >= 1) {
-			for (int i = 0; i < params.length; i++ ) {
-				Object param = params[i];
-
-				if (param != null) {
-					sb.append(param.toString());
-					sb.append(", ");
-				}
-			}
-			// Remove the last ', '
-			result = sb.substring(0, sb.length() - 2);
-		} else {
-			result = sb.toString();
-		}
-		System.out.println(result + ")");
-		return (result + ")");
 	}
 }
