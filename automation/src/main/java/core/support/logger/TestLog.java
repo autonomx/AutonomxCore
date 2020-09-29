fdb96017fb5091bd6319951b8d1dc7dae8cba92d
Ehsan Matean <ehsan.matean@fortify.pro>
2020-09-29 02:37:11 -0700

    fixed issue with maven version checker

--- automation/src/main/java/core/support/logger/TestLog.java
+++ automation/src/main/java/core/support/logger/TestLog.java
@@ -663,7 +663,7 @@ public class TestLog {
 			return checkLatestAutonomx();
 		}catch(Exception e) {
 			Config.putValue(TestLog.LOG_SKIP_CONSOLE, false, false);
-			System.out.println("Something has gone wrong with maven version check. To disable this message, set console.checkLatestAutonomx to false at report.property");
+			System.out.println("Something has gone wrong with maven version check. error <: "+ e.getMessage() +"> To disable this message, set console.checkLatestAutonomx to false at report.property");
 		}
 		return false;
 	}
